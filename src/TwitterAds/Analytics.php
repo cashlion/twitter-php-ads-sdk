514e4d12e87ff39b1e2c29b7cbaeda43e3a09b1c
Nicholas Hoium <nhoium@gmail.com>
2018-01-09 14:58:00 -0600

    Apply FSM modifications

--- src/TwitterAds/Analytics.php
+++ src/TwitterAds/Analytics.php
@@ -8,7 +8,6 @@
 
 namespace Hborras\TwitterAdsSDK\TwitterAds;
 
-
 use Hborras\TwitterAdsSDK\TwitterAds\Analytics\Job;
 use Hborras\TwitterAdsSDK\TwitterAds\Errors\BadRequest;
 use Hborras\TwitterAdsSDK\TwitterAds\Fields\AnalyticsFields;
@@ -35,47 +34,30 @@ class Analytics extends Resource
 
     public function all_stats($ids, $metricGroups, $params = [], $async = false)
     {
-        $endTime = isset($params[AnalyticsFields::END_TIME]) ? $params[AnalyticsFields::END_TIME] : new \DateTime('now');
-        $endTime->setTime($endTime->format('H'), 0, 0);
-        $startTime = isset($params[AnalyticsFields::START_TIME]) ? $params[AnalyticsFields::START_TIME] : new \DateTime($endTime->format('c') . " - 7 days");
-        $startTime->setTime($startTime->format('H'), 0, 0);
-        $granularity = isset($params[AnalyticsFields::GRANULARITY]) ? $params[AnalyticsFields::GRANULARITY] : Enumerations::GRANULARITY_TOTAL;
-        $placement = isset($params[AnalyticsFields::PLACEMENT]) ? $params[AnalyticsFields::PLACEMENT] : Enumerations::PLACEMENT_ALL_ON_TWITTER;
-        if (isset($params[AnalyticsFields::ENTITY])) {
-            $entity = $params[AnalyticsFields::ENTITY];
-        } else {
-            throw new BadRequest('Entity parameter is mandatory', 500, []);
-        }
-        if (!self::inAvailableEntities($entity)) {
-            throw new BadRequest('Entity must be one of ACCOUNT,FUNDING_INSTRUMENT,CAMPAIGN,LINE_ITEM,PROMOTED_TWEET,ORGANIC_TWEET', 500, []);
-        }
-        $segmentationType = isset($params[AnalyticsFields::SEGMENTATION_TYPE]) ? $params[AnalyticsFields::SEGMENTATION_TYPE] : null;
+        $endTime = isset($params['end_time']) ? $params['end_time'] : new \DateTime('now');
+        //$endTime->setTime($endTime->format('H'), 0, 0);
+        $startTime = isset($params['start_time']) ? $params['start_time'] : new \DateTime($endTime->format('c')." - 7 days");
+        //$startTime->setTime($startTime->format('H'), 0, 0);
+        $granularity = isset($params['granularity']) ? $params['granularity'] : Enumerations::GRANULARITY_HOUR;
+        $placement = isset($params['placement']) ? $params['placement'] : Enumerations::PLACEMENT_ALL_ON_TWITTER;
+        $segmentationType = isset($params['segmentation_type']) ? $params['segmentation_type'] : null;
+        $entity = $params['entity'];
+
 
         $params = [
             AnalyticsFields::METRIC_GROUPS => implode(",", $metricGroups),
-            AnalyticsFields::START_TIME => $startTime->format('c'),
-            AnalyticsFields::END_TIME => $endTime->format('c'),
+            AnalyticsFields::START_TIME => $startTime,
+            AnalyticsFields::END_TIME => $endTime,
             AnalyticsFields::GRANULARITY => $granularity,
             AnalyticsFields::ENTITY => $entity,
             AnalyticsFields::ENTITY_IDS => implode(",", $ids),
             AnalyticsFields::PLACEMENT => $placement,
         ];
 
-        if (!$async) {
-            $resource = str_replace(static::RESOURCE_REPLACE, $this->getTwitterAds()->getAccountId(), static::RESOURCE_STATS);
-            $response = $this->getTwitterAds()->get($resource, $params);
+        $resource = str_replace(static::RESOURCE_REPLACE, $this->getTwitterAds()->getAccountId(), static::RESOURCE_STATS);
+        $response = $this->getTwitterAds()->get($resource, $params);
 
-            return $response->getBody()->data;
-        } else {
-            if (!is_null($segmentationType)) {
-                $params[AnalyticsFields::SEGMENTATION_TYPE] = $segmentationType;
-            }
-
-            $resource = str_replace(static::RESOURCE_REPLACE, $this->getTwitterAds()->getAccountId(), static::RESOURCE_STATS_JOBS);
-            $response = $this->getTwitterAds()->post($resource, $params);
-            $job = new Job();
-            return $job->fromResponse($response->getBody()->data);
-        }
+        return $response->getBody()->data;
     }
 
     public static function inAvailableEntities($entity)
